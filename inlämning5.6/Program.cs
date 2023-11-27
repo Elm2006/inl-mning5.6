9c78ed5ba9a62edd86193d202942cae36bf2c1b2
Elm2006 <isel06002@utb.vaxjo.se>
2023-11-27 11:52:22 +0100

    Add project files.

---
+++ inlämning5.6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace inlämning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in tal");
+            int produkt = 1;
+            string svar = Console.ReadLine();
+            string[] faktorer = svar.Split('*');
+            for(int i = 0; i < faktorer.Length; i++)
+            {
+                produkt *= int.Parse(faktorer[i]);
+            }
+            Console.WriteLine(produkt);
+        }
+    }
+}
